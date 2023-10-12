26a097332e796b140ec469b56bf0d8e3b436a531
Thiago Jose <thiago.wilton2704@gmail.com>
2023-10-12 18:07:18 -0300

    Criação da class FilterTaskAuth

---
+++ todolist/src/main/java/br/com/thiagojose/todolist/filter/FilterTaksAuth.java
@@ -0,0 +1,22 @@
+package br.com.thiagojose.todolist.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class FilterTaksAuth implements Filter{
+
+    @Override
+    public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+        throws IOException, ServletException {
+        
+
+        //Execute alguma ação
+        
+    }
+    
+}
